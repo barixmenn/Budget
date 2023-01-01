c407ed0f3f1d182999a348109dced628f4a99d0e
baris dilekci <bdilekci200@gmail.com>
2023-01-01 20:34:06 +0300

    The process of displaying budgets has been completed.

--- Budget/Controller/CategoryTableViewController.swift
+++ Budget/Controller/CategoryTableViewController.swift
@@ -8,27 +8,41 @@
 import UIKit
 import CoreData
 
+
 class CategoryTableViewController: UITableViewController {
-    
+
     private var persistentContainer: NSPersistentContainer
+    private var fetchedResultsController: NSFetchedResultsController<BudgedCategory>!
     
     init(persistentContainer: NSPersistentContainer) {
         self.persistentContainer = persistentContainer
         super.init(nibName: nil, bundle: nil)
+        
+        let request = BudgedCategory.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
-        }
+        
+        // register cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BudgetTableViewCell")
+    }
     
-    //MARK: - Functions -
     @objc func showAddBudgetCategory(_ sender: UIBarButtonItem) {
         let navController = UINavigationController(rootViewController: AddBudgetViewController(persistentContainer: persistentContainer))
         present(navController, animated: true)
@@ -41,5 +55,31 @@ class CategoryTableViewController: UITableViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Budget"
     }
-   
+    
+    // UITableViewDataSource delegate functions
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (fetchedResultsController.fetchedObjects ?? []).count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BudgetTableViewCell", for: indexPath)
+        
+        let budgetCategory = fetchedResultsController.object(at: indexPath)
+        
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = budgetCategory.name
+        cell.contentConfiguration = configuration
+        
+        return cell
+    }
+
+}
+
+extension CategoryTableViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.  ()
+    }
+    
 }
